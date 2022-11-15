193e3725cee1ea58c1331a3bc3fab09bb0d5e320
parovstelar <113955451+parovstelar@users.noreply.github.com>
2022-11-15 06:11:28 +0000

    [finish] 4-24

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -4,7 +4,7 @@ class PostImagesController < ApplicationController
   end
 
   def create
-@post_image = PostImage.new(post_image_params)
+    @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
     if @post_image.save
       redirect_to post_images_path
@@ -14,7 +14,7 @@ class PostImagesController < ApplicationController
   end
 
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page])
   end
 
   def show
